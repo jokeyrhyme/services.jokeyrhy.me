f3836a2d1294de60c036714984152652e8f6c116
Ron Waldon <jokeyrhyme@gmail.com>
2014-03-08 11:27:20 +1100

    fix interface binding, and enable CORS

--- index.js
+++ index.js
@@ -5,7 +5,16 @@
 var Hapi = require('hapi');
 
 // Create a server with a host and port
-var server = Hapi.createServer('localhost', process.env.PORT || 8000);
+var server = Hapi.createServer('0.0.0.0', process.env.PORT || 8000, {
+  origin: [
+    'http://jokeyrhy.me',
+    'https://jokeyrhy.me',
+    'http://localhost:4000',
+    'https://localhost:4000'
+  ],
+  isOriginExposed: false,
+  credentials: false
+});
 
 // Add the route
 server.route({
